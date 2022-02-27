6363651a9805244f09f3cba433721b8c5d1a5f9c
Alex Wiles <alexandra.wiles6@gmail.com>
2022-02-26 22:43:02 -0500

    Added if statements to prompts and randomize results setting up

--- Develop/script.js
+++ Develop/script.js
@@ -6,21 +6,24 @@ var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@","\", ^", "_", "`", "{", "|", "}", "~"];
-// var passwordLength = randomNumber(7, 127);
+//var passwordLength = randomNumber(7, 127);
 
-// var randomNumber = function(min, max) {
-//   var value = Math.floor(Math.random() * (max - min + 1));
+//var randomNumber = function(min, max) {
+//var value = Math.floor(Math.random() * (max - min + 1));
+//console.log (randomNumber);
 
-//   return value;
-// };
+//return value;
+//};
 
-var prompts = function () {
+//var prompts = function () {
 
+function prompts () {
   // How long is password?
    var passwordLengthSelect =  window.prompt ("How long would you like your password between 8 and 128 characters?");
 
    if (passwordLengthSelect < 8 && passwordLengthSelect > 128) {
      window.prompt("Please choose a character between 8 and 128 characters");
+     return null;
    } else {
    }
 
@@ -65,14 +68,43 @@ var prompts = function () {
 
 
 // Generating the Actual Passwords
-function (generatePassword) {
-var runPassword = prompts();
-var passwordResults = [];
-var potentialResults = [];
-var actualResults = [];
+function generatePassword () {
+  //Randomizing Prompts results.
+  var runPrompts = prompts();
+  var passwordResults = [];
+  var potentialResults = [];
+  var actualResults = [];
 
-//Ability to call password results outside of function
-return generatePassword;
+  // Number Characters selected?
+  if (runPrompts.numbersConfirm) {
+    potentialResults = potentialResults.concat(numbers);
+        // If answer was yes, put into random generator.
+    actualResults.push(randomCharacters(numbers));
+  }  
+
+  //Uppercase Characters Selected?
+  if (runPrompts.uppercaseCharactersConfirm) {
+    potentialResults = potentialResults.concat(uppercase);
+        // If answer was yes, put into random generator.
+    actualResults.push(randomCharacters(uppercase));
+  }  
+
+  //Lowercase Characters Selected?
+  if (runPrompts.lowercaseCharactersConfirm) {
+    potentialResults = potentialResults.concat(lowercase);
+        // If answer was yes, put into random generator.
+    actualResults.push(randomCharacters(lowercase));
+  }  
+
+  //Special Characters Selected?
+  if (runPrompts.specialCharactersConfirm) {
+    potentialResults = potentialResults.concat(special);
+        // If answer was yes, put into random generator.
+    actualResults.push(randomCharacters(special));
+  }  
+
+  //Ability to call password results outside of function
+  return passwordResults.join('');
 };
   
 
@@ -86,8 +118,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 //Listener for the button click to write password. 
-
-
-
-//All answered, Password must generate that matches criteria.
-  //Once answered, must display in alert or written on page.
