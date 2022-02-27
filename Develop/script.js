7d96d70c6c489f317f907509aa7cf876a16cb12e
Alex Wiles <alexandra.wiles6@gmail.com>
2022-02-26 23:00:11 -0500

    Adjusting RAndom Number

--- Develop/script.js
+++ Develop/script.js
@@ -6,15 +6,15 @@ var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@","\", ^", "_", "`", "{", "|", "}", "~"];
-//var passwordLength = randomNumber(7, 127);
+var passwordLength = randomNumber(7, 127);
 
-//var randomNumber = function(min, max) {
-//var value = Math.floor(Math.random() * (max - min + 1));
-//console.log (randomNumber);
+var randomNumber = function(min, max) {
+var value = Math.floor(Math.random() * (max - min + 1));
+console.log (randomNumber);
 
-//return value;
-//};
-
+return value;
+};
+var passwordLength = randomNumber(7, 127);
 //var prompts = function () {
 
 function prompts () {
@@ -63,7 +63,6 @@ function prompts () {
 
   return passwordSelections;
     // If no selections, nothing to generate.
-
 };
 
 
